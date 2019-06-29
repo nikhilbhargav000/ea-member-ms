ce54a29df83743ceab40070b687173b66000c437
Nikhil Bhargav <nikhilbhargav000@gmail.com>
2019-06-29 14:35:43 +0530

    Rebasing

--- src/main/java/com/easyapper/member/controller/UserController.java
+++ src/main/java/com/easyapper/member/controller/UserController.java
@@ -23,128 +23,121 @@ import java.util.Set;
 @EnableAutoConfiguration
 @RequestMapping("/")
 public class UserController {
-    private final UserMgmntService userSrv;
-    private final GroupMgmntService groupService;
+	private final UserMgmntService userSrv;
+	private final GroupMgmntService groupService;
 
-    @Autowired
-    public UserController(UserMgmntService userSrv, GroupMgmntService groupService){
-        this.userSrv = userSrv;
-        this.groupService = groupService;
-    }
+	@Autowired
+	public UserController(UserMgmntService userSrv, GroupMgmntService groupService) {
+		this.userSrv = userSrv;
+		this.groupService = groupService;
+	}
 
-    @RequestMapping(value= "/{appId}/users", method = RequestMethod.POST)
-    public ResponseEntity<ResponseMessage> registerUser(@PathVariable("appId") String appId,
-                                                        @RequestBody Member user){
-        ResponseMessage resMsg = new ResponseMessage();
-        HttpStatus status = HttpStatus.CREATED;
-        if (user.getUserId() == null || user.getUserId().isEmpty() ||	//For Validation
-                user.getName() == null || user.getName().isEmpty()){
-            resMsg.setStatus("Failed");
-            resMsg.setMessage("Mandatory field missing");
-            status = HttpStatus.BAD_REQUEST;
-        } else if (userSrv.findMemberByUserId(appId, user.getUserId()) == null) {
-            ObjectId id = userSrv.createUser(appId, user);
+	@RequestMapping(value = "/{appId}/users", method = RequestMethod.POST)
+	public ResponseEntity<ResponseMessage> registerUser(@PathVariable("appId") String appId, @RequestBody Member user) {
+		ResponseMessage resMsg = new ResponseMessage();
+		HttpStatus status = HttpStatus.CREATED;
+		if (user.getUserId() == null || user.getUserId().isEmpty() || // For Validation
+				user.getName() == null || user.getName().isEmpty()) {
+			resMsg.setStatus("Failed");
+			resMsg.setMessage("Mandatory field missing");
+			status = HttpStatus.BAD_REQUEST;
+		} else if (userSrv.findMemberByUserId(appId, user.getUserId()) == null) {
+			ObjectId id = userSrv.createUser(appId, user);
 
-            resMsg.setId(id.toHexString());
-            resMsg.setStatus("Success");
-            resMsg.setMessage("User created successfully");
-        } else {
-            resMsg.setStatus("Failed");
-            resMsg.setMessage("userId already taken");
-            status = HttpStatus.CONFLICT;
-        }
+			resMsg.setId(id.toHexString());
+			resMsg.setStatus("Success");
+			resMsg.setMessage("User created successfully");
+		} else {
+			resMsg.setStatus("Failed");
+			resMsg.setMessage("userId already taken");
+			status = HttpStatus.CONFLICT;
+		}
 
-        return new ResponseEntity<>(resMsg, status);
-    }
-    
+		return new ResponseEntity<>(resMsg, status);
+	}
 
-    @RequestMapping(value= "/{appId}/users/{userId}", method = RequestMethod.GET)
-    public Member findMember(@PathVariable("appId") String appId, @PathVariable("userId") String userId){
-        Member member = userSrv.findMemberByUserId(appId, userId);
-        if(member == null) {
-            throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
-        }
-        //member.setGroups(null);
-        return member;
-    }
+	@RequestMapping(value = "/{appId}/users/{userId}", method = RequestMethod.GET)
+	public Member findMember(@PathVariable("appId") String appId, @PathVariable("userId") String userId) {
+		Member member = userSrv.findMemberByUserId(appId, userId);
+		if (member == null) {
+			throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
+		}
+		// member.setGroups(null);
+		return member;
+	}
 
-    @RequestMapping(value= "/{appId}/users", method = RequestMethod.GET)
-    public List<Member> findMembers(@PathVariable("appId") String appId){
-        return userSrv.findUsers(appId);
-    }
+	@RequestMapping(value = "/{appId}/users", method = RequestMethod.GET)
+	public List<Member> findMembers(@PathVariable("appId") String appId) {
+		return userSrv.findUsers(appId);
+	}    
     
     
     
     
-    
-    
-    @RequestMapping(value= "/{appId}/users/{userId}/groups", method = RequestMethod.POST)
-    public ResponseEntity<ResponseMessage> registerGroup(@PathVariable("appId") String appId,
-                                                         @PathVariable("userId") String userId,
-                                                         @RequestBody Group group){
-        ResponseMessage resMsg = new ResponseMessage();
-        HttpStatus status = HttpStatus.CREATED;
+	@RequestMapping(value = "/{appId}/users/{userId}/groups", method = RequestMethod.POST)
+	public ResponseEntity<ResponseMessage> registerGroup(@PathVariable("appId") String appId,
+			@PathVariable("userId") String userId, @RequestBody Group group) {
+		ResponseMessage resMsg = new ResponseMessage();
+		HttpStatus status = HttpStatus.CREATED;
 
-        if( group.getGroupId() == null || group.getGroupId().isEmpty() ||
-                userSrv.findMemberByUserId(appId, userId) == null || group.getCreatedBy() == null ||
-                (!"public".equals(group.getType())&& !"private".equals(group.getType()))){
-            resMsg.setStatus("Failed");
-            resMsg.setMessage("Mandatory field missing or invalid");
-            status = HttpStatus.BAD_REQUEST;
-        }else {
-            ObjectId id = groupService.createGroup(appId, group);
-            resMsg.setId(id.toHexString());
-            resMsg.setStatus("Success");
-            resMsg.setMessage("User Group created successfully");
-        }
+		if (group.getGroupId() == null || group.getGroupId().isEmpty()
+				|| userSrv.findMemberByUserId(appId, userId) == null || group.getCreatedBy() == null
+				|| (!"public".equals(group.getType()) && !"private".equals(group.getType()))) {
+			resMsg.setStatus("Failed");
+			resMsg.setMessage("Mandatory field missing or invalid");
+			status = HttpStatus.BAD_REQUEST;
+		} else {
+			ObjectId id = groupService.createGroup(appId, group);
+			resMsg.setId(id.toHexString());
+			resMsg.setStatus("Success");
+			resMsg.setMessage("User Group created successfully");
+		}
 
-        return new ResponseEntity<>(resMsg, status);
-    }
+		return new ResponseEntity<>(resMsg, status);
+	}
 
-    @RequestMapping(value= "/{appId}/users/{userId}/groups/{groupId}", method = RequestMethod.PUT)
-    public ResponseEntity<ResponseMessage> addGroupMembers(@PathVariable("appId") String appId,
-                                                           @PathVariable("groupId") String groupId,
-                                                           @RequestBody List<Member> members){
-        ResponseMessage resMsg = new ResponseMessage();
-        HttpStatus status = HttpStatus.OK;
+	@RequestMapping(value = "/{appId}/users/{userId}/groups/{groupId}", method = RequestMethod.PUT)
+	public ResponseEntity<ResponseMessage> addGroupMembers(@PathVariable("appId") String appId,
+			@PathVariable("groupId") String groupId, @RequestBody List<Member> members) {
+		ResponseMessage resMsg = new ResponseMessage();
+		HttpStatus status = HttpStatus.OK;
 
-        Group grp = groupService.findGroup(appId, groupId);
-        if(grp == null || !grp.getGroupId().equals(grp.getGroupId())){
-            resMsg.setStatus("Failed");
-            resMsg.setMessage("Mandatory field missing or invalid");
-            status = HttpStatus.BAD_REQUEST;
-        }else{
-            groupService.addGroupMembers(appId, grp, members);
-            resMsg.setId(null);
-            resMsg.setStatus("Success");
-            resMsg.setMessage("Group members added successfully");
-        }
+		Group grp = groupService.findGroup(appId, groupId);
+		if (grp == null || !grp.getGroupId().equals(grp.getGroupId())) {
+			resMsg.setStatus("Failed");
+			resMsg.setMessage("Mandatory field missing or invalid");
+			status = HttpStatus.BAD_REQUEST;
+		} else {
+			groupService.addGroupMembers(appId, grp, members);
+			resMsg.setId(null);
+			resMsg.setStatus("Success");
+			resMsg.setMessage("Group members added successfully");
+		}
 
-        return new ResponseEntity<>(resMsg, status);
-    }
+		return new ResponseEntity<>(resMsg, status);
+	}
 
+	@RequestMapping(value = "/{appId}/users/{userId}/groups", method = RequestMethod.GET)
+	public List<Group> findUserGroups(@PathVariable("appId") String appId, @PathVariable("userId") String userId) {
+		Set<Group> groups = new HashSet<>();
+		if (userSrv.findMemberByUserId(appId, userId) == null) {
+			throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
+		} else {
+			groups.addAll(userSrv.findUserGroups(appId, userId));
+		}
 
-    @RequestMapping(value= "/{appId}/users/{userId}/groups", method = RequestMethod.GET)
-    public List<Group> findUserGroups(@PathVariable("appId") String appId, @PathVariable("userId") String userId){
-        Set<Group> groups = new HashSet<>();
-        if( userSrv.findMemberByUserId(appId, userId) == null){
-            throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
-        }else{
-            groups.addAll(userSrv.findUserGroups(appId, userId));
-        }
+		groups.addAll(groupService.findGroupsByType("public"));
 
-        groups.addAll(groupService.findGroupsByType("public"));
+		return new ArrayList<>(groups);
+	}
 
-        return new ArrayList<>(groups);
-    }
-
-    @RequestMapping(value= "/{appId}/users/{userId}/groups/{groupId}", method = RequestMethod.GET)
-    public Group findUserGroup(@PathVariable("appId") String appId, @PathVariable("userId") String userId,
-              @PathVariable("groupId") String groupId){
-        if( userSrv.findMemberByUserId(appId, userId) == null){
-            throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
-        }else{
-            return groupService.findGroup(appId, groupId);
-        }
-    }
+	@RequestMapping(value = "/{appId}/users/{userId}/groups/{groupId}", method = RequestMethod.GET)
+	public Group findUserGroup(@PathVariable("appId") String appId, @PathVariable("userId") String userId,
+			@PathVariable("groupId") String groupId) {
+		if (userSrv.findMemberByUserId(appId, userId) == null) {
+			throw new MemberRuntimeException(ErrorCode.NOT_FOUND, "user does not exist");
+		} else {
+			return groupService.findGroup(appId, groupId);
+		}
+	}
 }
--- src/main/java/com/easyapper/member/model/Member.java
+++ src/main/java/com/easyapper/member/model/Member.java
@@ -8,77 +8,77 @@ import java.util.List;
 import java.util.Objects;
 
 public class Member {
-    @Id
-    private ObjectId id;
-    private String userId;
-    private String name;
-    private List<Group> groups = new ArrayList<>();
+	@Id
+	private ObjectId id;
+	private String userId;
+	private String name;
+	private List<Group> groups = new ArrayList<>();
 
-    private long createdAt;
-    private boolean isActive;
+	private long createdAt;
+	private boolean isActive;
 
-    public ObjectId getId() {
-        return id;
-    }
+	public ObjectId getId() {
+		return id;
+	}
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
 
-    public String getUserId() {
-        return userId;
-    }
+	public String getUserId() {
+		return userId;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    public List<Group> getGroups() {
-        return groups;
-    }
+	public List<Group> getGroups() {
+		return groups;
+	}
 
-    public void setGroups(List<Group> groups) {
-        this.groups = groups;
-    }
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public long getCreatedAt() {
-        return createdAt;
-    }
+	public long getCreatedAt() {
+		return createdAt;
+	}
 
-    public void setCreatedAt(long createdAt) {
-        this.createdAt = createdAt;
-    }
+	public void setCreatedAt(long createdAt) {
+		this.createdAt = createdAt;
+	}
 
-    public boolean isActive() {
-        return isActive;
-    }
+	public boolean isActive() {
+		return isActive;
+	}
 
-    public void setActive(boolean active) {
-        isActive = active;
-    }
+	public void setActive(boolean active) {
+		isActive = active;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Member member = (Member) o;
-        return getCreatedAt() == member.getCreatedAt() &&
-                Objects.equals(getId(), member.getId()) &&
-                Objects.equals(getUserId(), member.getUserId()) &&
-                Objects.equals(getName(), member.getName());
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Member member = (Member) o;
+		return getCreatedAt() == member.getCreatedAt() && Objects.equals(getId(), member.getId())
+				&& Objects.equals(getUserId(), member.getUserId()) && Objects.equals(getName(), member.getName());
+	}
 
-    @Override
-    public int hashCode() {
+	@Override
+	public int hashCode() {
 
-        return Objects.hash(getId(), getUserId(), getName(), getCreatedAt());
-    }
+		return Objects.hash(getId(), getUserId(), getName(), getCreatedAt());
+	}
 }
